b1708067b86d7dd9bbb1e1bd09ba5da01025f08b
Sandesh Jain <sandeshjain2507@gmail.com>
2021-11-09 15:09:02 +0530

    Deployment Changes - Heroku

--- aspirer/settings.py
+++ aspirer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1&_f&7k9eil)_8p&9q(u1=wkyudry8u0(t+d23z&&)0bns_6et'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['aspirer-app.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
